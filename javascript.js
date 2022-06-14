5a7167c0dba0bb4a270302097d0dc484ef35c87c
Hannah.Celemen <hcelemen@icloud.com>
2022-06-13 20:37:01 -0600

    Initial Commit

---
+++ javascript.js
@@ -0,0 +1,9 @@
+//We named the file javascript because we are practicing javascript.
+var favoriteBook = "Harry Potter and the Sorcerer's Stone";
+var numberOfBooks = 5; //we chose to assign this value because that's how many books we had listed in the .md file
+var isFiction = true; //we chose to name the boolean variable this to pose it as a yes/no question
+var isOld = false; //
+var isMovie = true; //we chose this variable because we had difficulty thinking of books, so we are figuring out what books have been turned into a movie
+var library = "Denver Public Library"; //we chose this variable for the library that potentially has these books
+
+var books = ["Harry Potter and the Sorcerer's Stone", "Apothecary", "Twilight", "Dune", "Little Women"];
